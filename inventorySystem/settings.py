7244e32e4c83fab4788bea7af22e042bb4d479d2
ahassouna <ahassouna@softstrategy.it>
2023-02-12 17:38:34 +0100

    final

--- inventorySystem/settings.py
+++ inventorySystem/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = 'django-insecure-^wwq&c-541wrk*%1i&x%j%ncef3&w=lfq=ql#&zvm(zzuto*0l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.17')]
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.17')]
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
